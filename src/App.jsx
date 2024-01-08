e8976804d3471ae8ead0a112e49bd1f56a52175e
jasin1 <jasin.tairaidrissi@gmail.com>
2024-01-08 10:07:09 +0100

    eerste opzet

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import {useState} from 'react'
 import './App.css'
+import trashCan from './assets/trash-can.svg'
+import clockIcon from './assets/clock_icon.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <main>
+                <header>
+                    <h1>Todo App</h1>
+                </header>
+                <article>
+                    <section>
+                        <form action="">
+                            <div className="task-submit">
+                                <label htmlFor="">
+                                    <input type="text"/>
+                                    <div className="btn-wrap">
+                                        <button>
+                                            <img src={clockIcon} alt=""/>
+                                        </button>
+                                        <button type="submit"> Add</button>
+                                    </div>
+                                </label>
+                            </div>
+
+                            <div className="task-added">
+                                <ul>
+                                    <li>
+                                        <label htmlFor="">
+                                            <div className="cheking">
+                                            <input type="checkbox"/>
+                                            <span>Drink water</span>
+                                            </div>
+                                            <div>
+                                                <span>7:55 PM</span>
+                                                <button>
+                                                    <img src={trashCan} alt=""/>
+                                                </button>
+
+                                            </div>
+                                        </label>
+                                    </li>
+                                </ul>
+                            </div>
+                        </form>
+                    </section>
+                    <section>
+
+                    </section>
+                </article>
+
+
+            </main>
+        </>
+    )
 }
 
 export default App
